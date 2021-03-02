624aa062a7b81047be53351a42b819f4e0f35caf
Alexandru Crisan <calex2005cj@gmail.com>
2021-03-02 16:22:54 +0200

    algorithm improvements

--- index.js
+++ index.js
@@ -1,59 +1,44 @@
-const convertapi = require('convertapi')('zFrNRK89Ja3Osmdj');
-const path = `./FilesToBeConverted/`;
-const readline = require('readline');
+const convertapi = require('convertapi')('token');
+const default_path = `./FilesToBeConverted/`;
 const fs = require('fs');
 
+var final_file;
+
+var all_the_names = [];
+var index = 0;
+
+const allFiles = fs.readdirSync(default_path);
+
+for(const file of allFiles){
+  all_the_names[index] = file;
+  index++;
+}
+if(index == 0){
+  console.log("make sure you added at least one file !");
+  return 0;
+}
 
 
+for(var i = 0; i<index; i++){
+  conv(all_the_names[i]);
+}
 
-
-
-
-const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout
-});
-
-var completed_path = "";
-var file_name = "";
-var file_ext_in = "";
-var file_ext_ult = ".pdf";
-var path_without_ext = '';
-var final_path = "";
-var finp = "";
-
-var toate_nume = [100];
-
-var i = 0;
-
-let temp = fs.readdirSync(path);
-
-temp.forEach((f) => {
-  file_name = f;
-})
-
-final_path = path + file_name;
-
-conv();
-
-function conv(){
-  convertapi.convert('pdf', { File: final_path })
+function conv(file_name){
+  
+  convertapi.convert('pdf', { File: default_path + file_name })
     .then(function(result) {
       // get converted file url
-      
-
+    
       console.log("Converted file url: " + result.file.url);
-
       
-      finp = path + file_name.slice(0, file_name.length - 5) + ".pdf";
-      console.log(finp);
+      final_file = default_path + file_name.slice(0, file_name.length - 5) + ".pdf";
+      console.log(final_file);
 
       // save to file
-      return result.file.save(finp);
+      return result.file.save(final_file);
     })
     .then(function(file) {
       console.log("File saved: " + file);
-      process.exit(1);
     })
     .catch(function(e) {
       console.log("numele si/sau extensia fisierului sunt gresite");
@@ -61,64 +46,3 @@ function conv(){
       process.exit(1);
   });
 }
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////
-
-/*rl.question('nume? ', (answer) => {
-  file_name = answer;
-
-  rl.question('extensie? ',(answer2) => {
-    file_ext_in = answer2;
-    function path_maker () {
-      var local_path;
-        
-      path_without_ext = path + file_name;
-      local_path = path_without_ext + file_ext_in;
-        
-      return local_path;
-    }
-    completed_path = path_maker();
-  
-    convertapi.convert('pdf', { File: completed_path })
-    .then(function(result) {
-      // get converted file url
-      console.log("Converted file url: " + result.file.url);
-  
-      // save to file
-      return result.file.save(path_without_ext + file_ext_ult);
-    })
-    .then(function(file) {
-      console.log("File saved: " + file);
-    })
-    .catch(function(e) {
-      console.log("numele si/sau extensia fisierului sunt gresite");
-      process.exit(1);
-    });
-    rl.close();
-
-  })
-  
-});
-  
-
-    
-    
-
-    
-
-    
-  
-
-
-//readline.question('extensie : ', file_ext_in);
-
-//console.log("numele fisierului : ");
-//readline(file_name);
-//console.log("extensie : ");
-//readline(file_ext_in);*/
-
-
